f1b480beae643a99982b821e36d94b69719f29d9
Junyoung <wnsdud12365@naver.com>
2023-02-05 16:48:25 +0900

    ComponentScan 범위 및 대상 지정

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // Component Scan 범위 지정
+        // 여러 개도 한번에 가능
+        // Default범위는 : 현재 AutoAppConfig가 있는 패키지부터 (hello.core부터 전부)
+        // 권장 : 프로젝트 최상단에 AutoAppConfig 파일을 위치시킨다. -> 스프링 부트에서도 해당 방법 제공
+        basePackages = {"hello.core", "hello.core.member"},
         // Component Scan도중 제외할 것들
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
         classes = Configuration.class))
