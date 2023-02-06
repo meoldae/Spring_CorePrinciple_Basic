7fcedc9383df73c4087f46520bcda56a7bdedc42
Junyoung <wnsdud12365@naver.com>
2023-02-06 22:22:18 +0900

    Lombok을 통한 편의성 강화

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,28 +3,20 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
-    // 생성자 주입 시 final 키워드 사용 가능!
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // 역시 인터페이스(역할)에만 의존함!
-    // 누군가 "주입"해주면 그것에 대해 로직만 실행하면 됨
-    // MemoryMemberRepo + RateDiscountPolicy 가 주입된다.
-    // @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-//    일반 메서드 주입
+// lombok이 만들어줌 !
 //    @Autowired
-//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
