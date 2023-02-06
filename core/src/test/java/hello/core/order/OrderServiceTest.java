8e418da8f49d87226adb36cdf68620736f9c35ef
Junyoung <wnsdud12365@naver.com>
2023-02-06 21:24:45 +0900

    다양한 의존관계 주입 방법

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -31,5 +31,12 @@ public class OrderServiceTest {
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
+    @Test
+    void fieldInjectionTest(){
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//        orderService.createOrder(1L, "itemA", 10000);
+    }
+
+
 }
 
