c9ca6cd659d71ac0a86cf74e204b17b99866146b
Junyoung <wnsdud12365@naver.com>
2023-02-06 21:54:30 +0900

    생성자 주입의 장점

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,29 +9,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    // DIP, OCP 위배
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // discountPolicy = new RateDiscountPolicy();
-
-    // Interface만 ? => NullPointerException
-    private DiscountPolicy discountPolicy;
+    // 생성자 주입 시 final 키워드 사용 가능!
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     // 역시 인터페이스(역할)에만 의존함!
     // 누군가 "주입"해주면 그것에 대해 로직만 실행하면 됨
     // MemoryMemberRepo + RateDiscountPolicy 가 주입된다.
-//    @Autowired
+    // @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
-    // 일반 메서드 주입
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    일반 메서드 주입
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+          /*
+          순수 자바코드 테스트의 경우 스프링 DI의 도움을 받지 못함
+          임의로 가짜 객체를 넣어서 테스트 가능
+           */
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
