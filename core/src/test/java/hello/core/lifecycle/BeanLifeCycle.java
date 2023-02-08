9744914e1a419b51684e9ef7cc2ac80fd586880e
Junyoung <wnsdud12365@naver.com>
2023-02-08 22:27:19 +0900

    PostConstruct, PreDestroy의 사용

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycle.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycle.java
@@ -20,7 +20,7 @@ public class BeanLifeCycle {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
