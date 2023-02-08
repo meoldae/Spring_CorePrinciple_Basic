74718b1162fcfb61221b0a87eeee623bb011d6fb
Junyoung <wnsdud12365@naver.com>
2023-02-08 21:36:38 +0900

    빈 등록시 수동과 자동의 사용 기준

---
+++ core/src/main/java/hello/core/DiscountPolicyConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+다른 사람이 코드를 확인해도 한 눈에 들어오는 정보
+ApplicationContext에 DiscountPolicyConfig.class를 등록하자.
+ */
+@Configuration
+public class DiscountPolicyConfig {
+    @Bean
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
