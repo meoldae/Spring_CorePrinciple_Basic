d94bdacdbe70cb680cf798ceaad30608807f0e40
Junyoung <wnsdud12365@naver.com>
2023-02-11 18:19:03 +0900

    Proxy를 통한 지연 처리

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,17 +2,14 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    // private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> loggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = loggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
