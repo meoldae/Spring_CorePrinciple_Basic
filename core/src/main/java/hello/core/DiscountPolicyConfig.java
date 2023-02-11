54d32dac6738592fc2b508bc4b8741edfc2a5562
Junyoung <wnsdud12365@naver.com>
2023-02-11 18:06:22 +0900

    Web Scope 빈과 Provider 사용

--- core/src/main/java/hello/core/DiscountPolicyConfig.java
+++ core/src/main/java/hello/core/DiscountPolicyConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 다른 사람이 코드를 확인해도 한 눈에 들어오는 정보
 ApplicationContext에 DiscountPolicyConfig.class를 등록하자.
  */
+/*실행시 오류 제거를 위함
 @Configuration
 public class DiscountPolicyConfig {
     @Bean
@@ -20,4 +21,4 @@ public class DiscountPolicyConfig {
     public DiscountPolicy fixDiscountPolicy() {
         return new FixDiscountPolicy();
     }
-}
+}*/
